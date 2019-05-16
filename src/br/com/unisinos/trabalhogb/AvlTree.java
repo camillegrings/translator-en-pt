67d43c4eadc5a8a19e43f38ff918c04304a194b4
Camille Grings Silva <camille.silva@cwi.com.br>
2019-05-16 18:57:06 -0300

    refactor: comentarios avlTree

--- src/br/com/unisinos/trabalhogb/AvlTree.java
+++ src/br/com/unisinos/trabalhogb/AvlTree.java
@@ -7,94 +7,113 @@ public class AvlTree {
 
 	private AvlNode root = null;
 
-	public AvlTree(  ) {
+	public AvlTree() {
 		root = null;
 	}
 
-	public void clear() {
-		root = null;
-	}
-	public boolean isEmpty() {
-		return root == null;
-	}
+//	public void clear() {
+//		root = null;
+//	}
+//	public boolean isEmpty() {
+//		return root == null;
+//	}
+//
+//	public AvlNode getRootNode (){
+//		return root;
+//	}
 
-	public AvlNode getRootNode (){
-		return root;
-	}
-
-	private static int height( AvlNode t ) {
-		if (t == null)
-			return -1;
+	private static int height( AvlNode node ) {
+		if (node == null)
+			return -1; //Se o nodo é null, retorna -1
 		else
-			return t.height;
+			return node.height;
 	}
 
-	private static int max( int lhs, int rhs ) {
-		if (lhs > rhs)
-			return lhs;
+	private static int getMaior( int leftNode, int rightNode ) {
+		if (leftNode > rightNode)
+			return leftNode;
 		else
-			return rhs;
+			return rightNode;
 	}
 
-	private int getFactor (AvlNode t) {
-		return height( t.left ) - height( t.right );
+	private int getFactor (AvlNode node) {
+		//Se node for null, left retorna -1, right retorna -1, na expressão (-1)-(-1) = 0
+		//Se algum dos nodes estiver preenchido, usa o seu atributo de altura
+		return height( node.left ) - height( node.right );
 	}
-
+	
 	public boolean insert (Dicionario d) {
 		root = insert (d, root);
 		return true;
 	}
 
-	private AvlNode insert (Dicionario d, AvlNode t) {
-		if( t == null ) {
-			t = new AvlNode( d, null,null);
+	private AvlNode insert (Dicionario d, AvlNode node) {
+		//Se o node for null, instancia um node
+		if( node == null ) {
+			node = new AvlNode( d, null,null);
 		}
-		else if( d.compareTo(t.Key) > 0 ) { 
-			t.left = insert( d, t.left );
+		//Compara a key do objeto a ser inserido com a key já existente na árvore:
+		//Se a chave do objeto for > 0, significa que a chave do objeto a ser inserido é menor que a chave existente,
+		//portanto, insere a esquerda
+		else if( d.compareTo(node.Key) > 0 ) { 
+			node.left = insert( d, node.left );
 		}
-		else if( d.compareTo(t.Key) < 0) {
-			t.right = insert( d, t.right );
+		//Se a chave do objeto for < 0, a chave do objeto a ser inserido é maior que a chave existente, logo insere a direita
+		else if( d.compareTo(node.Key) < 0) {
+			node.right = insert( d, node.right );
 		}
-
-		if ( getFactor(t) == 2 ) {
-			if (getFactor (t.left)>0) t = doRightRotation( t );
-			else t = doDoubleRightRotation( t );
+        //Após realizada a operação de inserção, verifica o fator do node para determinar se a árvore está balanceada
+		if ( getFactor(node) == 2 ) { //Se o fator for 2, a árvore não está balanceada
+			if (getFactor (node.left)>0) //Sendo o fator da sub-arvore à esquerda positivo, faz rotação simples à direita
+				node = rotateSimpleToRight( node );
+			else // Senão, faz rotação dupla à direita
+				node = rotateDoubleToRight( node );
 		}
-		else if ( getFactor(t) == -2 ) {
-			if ( getFactor(t.right)<0 ) t = doLeftRotation( t );
-			else t = doDoubleLeftRotation( t );
+		else if ( getFactor(node) == -2 ) {//Se o fator for -2, a árvore não está balanceada
+			if ( getFactor(node.right)<0 ) //Sendo o fator da sub-arvore à direita negativo, faz rotação simples à direita
+				node = rotateSimpleToLeft( node );
+			else  // Senão, faz rotação dupla à esquerda
+				node = rotateDoubleToLeft( node );
 		}
-
-		t.height = max( height( t.left ), height( t.right ) ) + 1;  
-		return t;
+        
+		//Determina a altura do nodo, considerando a maior das alturas entre left e rigth + 1
+		/*
+		                |4| (2+1) ->3, maior altura entre a sub arvore da esquerda e direita + 1
+		                 |
+		       |2| (1+1)       |5| (0+1)
+		        |
+		     |1| |3| (0+1)     
+		 */
+		node.height = getMaior( height( node.left ), height( node.right ) ) + 1;  
+		return node;
 	}
 
-	private static AvlNode doRightRotation( AvlNode k2 ) {
-		AvlNode k1 = k2.left;
-		k2.left = k1.right;
-		k1.right = k2;
-		k2.height = max( height( k2.left ), height( k2.right ) ) + 1;
-		k1.height = max( height( k1.left ), k2.height ) + 1;
-		return k1;
+	private static AvlNode rotateSimpleToRight( AvlNode k2 ) {
+		AvlNode k1 = k2.left;//Filho esquerdo de K2 vira pai 
+		k2.left = k1.right;  //Filho direito de k1 vira filho esquerdo de k2
+		k1.right = k2;       //k2 vira filho direito de k1
+		k2.height = getMaior( height( k2.left ), height( k2.right ) ) + 1; // A altura de k2 é definida pela maior altura entre suas sub arvores direita e esquerda, + 1
+		k1.height = getMaior( height( k1.left ), k2.height ) + 1; //A altura de k1 é determinada pela maior altura entre a altura de k2 e a altura da sub arvore esquerda de k1
+		return k1; //Retorna o node rotacionado
 	}
 
-	private static AvlNode doLeftRotation( AvlNode k1 ) {
+	private static AvlNode rotateSimpleToLeft( AvlNode k1 ) {
 		AvlNode k2 = k1.right;
 		k1.right = k2.left;
 		k2.left = k1;
-		k1.height = max( height( k1.left ), height( k1.right ) ) + 1;
-		k2.height = max( height( k2.right ), k1.height ) + 1;
+		k1.height = getMaior( height( k1.left ), height( k1.right ) ) + 1;
+		k2.height = getMaior( height( k2.right ), k1.height ) + 1;
 		return k2;
 	}
 
-	private static AvlNode doDoubleRightRotation( AvlNode k3 ) {
-		k3.left = doLeftRotation( k3.left );
-		return doRightRotation( k3 );
+	private static AvlNode rotateDoubleToRight( AvlNode k3 ) {
+		k3.left = rotateSimpleToLeft( k3.left );
+		return rotateSimpleToRight( k3 );
 	}
 
-	private static AvlNode doDoubleLeftRotation( AvlNode k1 ) {
-		k1.right = doRightRotation( k1.right );
-		return doLeftRotation( k1 );
+	private static AvlNode rotateDoubleToLeft( AvlNode k1 ) {
+		k1.right = rotateSimpleToRight( k1.right );
+		return rotateSimpleToLeft( k1 );
 	}
 
 	/*public AvlNode search(String palavra) {
