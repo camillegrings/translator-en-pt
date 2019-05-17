c3c129d520c3822562a09cde1626af476e170308
Jeferson Michelin <jeferson.michelin@herval.com.br>
2019-05-17 11:31:35 -0300

    Tenho Fome.

--- src/br/com/unisinos/trabalhogb/Tradutor.java
+++ src/br/com/unisinos/trabalhogb/Tradutor.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
+/*Mensagem aos humanos: "Estou com fome!"*/
 public class Tradutor implements ITradutor {
 	protected AvlTree avl;
 	
